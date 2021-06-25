user202729/plover-search-translation

commit 05f45ce7d47ffa52e60a839b2991299359de10e6

Optimize performance -- do not call search repeatedly

plover_search_translation/process.py
@@ -21,7 +21,7 @@ import html
 
 from subprocess_connection import Message
 
-from .lib import Entry
+from .lib import Entry, throttle
 from .gui import SearchTranslationDialog
 
 from PyQt5.QtCore import pyqtSignal, QVariant, QObject
@@ -250,6 +250,8 @@ def delete_translation()->None:
 
 dialog.deleteButton.clicked.connect(delete_translation)
 
+@throttle(0.05)
+@execute_on_main_thread
 def description_search_changed(text: str)->None:
 	if state is PROGRAMMATICALLY_EDITING_DESCRIPTION or isinstance(state, Editing):
 		return

plover_search_translation/lib.py
@@ -1,4 +1,6 @@
 import functools
+import time
+import threading
 import typing
 from typing import Tuple, Dict, List, Optional, TypeVar, Callable, Sequence, Any
 from dataclasses import dataclass
@@ -49,3 +51,43 @@ class Entry:  # field order is important
 				description=data[1],
 				brief=tuple(data[2])
 				)
+
+
+def throttle(seconds: float)->Callable[[T], T]:
+	"""
+	Wait for <seconds> seconds, collect all the function calls, then only call the last function.
+
+	The delay is only approximate, may be longer if the actual function call takes a long time.
+
+	Example:
+
+	Time                    ---------------------------------------------> (s)
+	Wrapped function call:    * *          *  * * ***     * *              ('*' = 1 call)
+	Actual function call:     .......#     .......#.......#.......#        ('#' = 1 call)
+	                          \______/
+	                             |_______ ==  delay time
+	"""
+
+	def result(function: T)->T:
+		thread=None
+		function_args, function_kwargs=None, None
+		lock=threading.Lock()  # should be locked when the function is called or when any of the variables above is modified
+
+		def target(*args, **kwargs):
+			nonlocal thread
+			time.sleep(seconds)
+			with lock:
+				thread=None
+				function(*function_args, **function_kwargs)
+
+		@functools.wraps(function)
+		def wrapped(*args, **kwargs)->Any:
+			nonlocal thread, function_args, function_kwargs
+			with lock:
+				function_args, function_kwargs=args, kwargs
+				if thread is None:
+					thread=threading.Timer(seconds, target)
+					thread.start()
+
+		return typing.cast(T, wrapped)
+	return result

commit 4234da214e5167d6d5d36bff9934ca5755632cb0

Fix bug: set_description_text may repopulate matches table
Caused by commit 05f45ce7d47ffa52e60a839b2991299359de10e6.

plover_search_translation/process.py
@@ -252,6 +252,14 @@ dialog.deleteButton.clicked.connect(delete_translation)
 
 @throttle(0.05)
 @execute_on_main_thread
+def repopulate_matches(query: str)->None:
+	if state is WINDOW_CLOSED:
+		return
+	result: List[Entry] = message.func.search(query)
+	dialog.matches.setRowCount(len(result))
+	for row, entry in enumerate(result):
+		dialog.set_row_data(row, entry)
+
 def description_search_changed(text: str)->None:
 	if state is PROGRAMMATICALLY_EDITING_DESCRIPTION or isinstance(state, Editing):
 		return
@@ -259,11 +267,7 @@ def description_search_changed(text: str)->None:
 		# this might happen if the description text is modified right before the dialog is closed
 		return
 	assert state is WINDOW_OPEN, state
-
-	result: List[Entry] = message.func.search(text)
-	dialog.matches.setRowCount(len(result))
-	for row, entry in enumerate(result):
-		dialog.set_row_data(row, entry)
+	repopulate_matches(text)
 
 def brief_changed(text: str)->None:
 	outline=text_to_outline(text)